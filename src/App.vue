b88bc652556b932cd2cf5f3c996ac63a4dfaaf6a
tanglei <tanglei128@126.com>
2019-05-28 17:42:24 +0800

    1.迁移代码

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+  export default {
+    created () {
+      console.log('miniapp created!!!')
+    }
+  }
+</script>
+
+<style>
+  /* 这部分相当于原生小程序的 app.wxss */
+  .container {
+    background-color: #cccccc;
+  }
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <p>{{msg}}</p>
+      <button @click="clickMe">点击</button>
+      <i-button type="primary" @click="clickMe">这是一个按钮</i-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+      data(){
+          return{
+            msg:'hello'
+          }
+      },
+      methods:{
+        clickMe(){
+            this.msg =  new Date().getTime() + '';
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
